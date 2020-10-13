f0ce63a9d0e00ab5f77e965d16c63f4a941f3a26
MatVini <matvcirino@gmail.com>
2020-10-13 15:57:53 -0300

    Arquivo principal

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,6 @@ const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
 	width: 800,
 	height: 600,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 300 },
-			debug: true
-		}
-	},
 	// scene: [HelloWorldScene]
 	scene: [MainGame]
 }
--- src/scenes/MainGame.ts
+++ src/scenes/MainGame.ts
@@ -2,6 +2,8 @@ import Phaser from 'phaser'
 
 export default class MainGameScene extends Phaser.Scene
 {
+    private greeting?: Phaser.GameObjects.Text
+
 	constructor()
 	{
 		super('hello-world')
@@ -15,6 +17,12 @@ export default class MainGameScene extends Phaser.Scene
     create()
     {
         this.add.image(400, 300, 'sky')
+
+        this.greeting = this.add.text(250, 150, 'Hello World!', {
+            fontFamily: 'Arial',
+            fontSize: '64px',
+            fill: '#000'
+        })
     }
 
     update()
